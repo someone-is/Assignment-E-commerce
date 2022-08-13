b25deae9a28e150edb8a4dcc2c7204e3c184c909
someone-is <shouryashekhar.31december@gmail.com>
2022-08-13 23:26:51 +0530

    cart

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -5,17 +5,18 @@ import ContentItem from "./ContentItem.js";
 import Navigation from './Navigation.js';
 import Content2 from './Content2';
 import ShoppingCart from "./ShoppingCart.js";
-import { Toaster,toast } from "react-hot-toast";
+import { Toaster, toast } from "react-hot-toast";
+
 export const CartContext = createContext();
 const initialState = {
     item: products,
     totalAmount: 0,
     totalItem: 0,
-    AddTCart:[],
+    item_cart: [],
 };
 const Content = () => {
-const [cart, setcart] = useState(false);
-const showCart = () => setcart(!cart);
+    const [cart, setcart] = useState(false);
+    const showCart = () => setcart(!cart);
 
     const [state, dispatch] = useReducer(reducer, initialState);
     const IncreMent = (id) => {
@@ -25,10 +26,11 @@ const showCart = () => setcart(!cart);
         });
     };
 
-    const DecereSe = (id) => {
+    const DecereSe = (id, setbutton) => {
         return dispatch({
             type: "DECERESE",
             payload: id,
+            buttns: setbutton,
         });
     };
     const AddToCart = (id) => {
@@ -37,39 +39,76 @@ const showCart = () => setcart(!cart);
             payload: id,
         });
     };
-    const DecrementCart =(id) =>{
+    const DecrementCart = (id) => {
         return dispatch({
             type: "CARTDEC",
             payload: id,
         });
     };
-    const removefromCart =(id) =>{
+    const removefromCart = (id) => {
         return dispatch({
             type: "DELETE",
             payload: id,
         });
     }
-    
+
     useEffect(() => {
         dispatch({ type: "TOTAL" });
-    }, [state.item]);
-    const [Addcart,setAddcart] = useState([]);
-    const clicktocart=(itm)=>{
-        Addcart.push(itm);
-        console.log(Addcart);
+    }, [state.item, state.item_cart]);
+    const clicktocart = (itm, setbutton) => {
+        state.item_cart.push(itm);
+        console.log(state.item_cart);
+        state.totalItem = itm.no;
+        setbutton(false)
         toast.success(`${itm.name} added to the cart`);
-        if (Addcart.indexOf(itm)!== -1) return;
-        else{setAddcart([...Addcart,itm]);}
 
+        // const [Addcart, setAddcart] = useState(cartproducts);
+        // console.log(Addcart);
+        // if (Addcart.indexOf(itm) !== -1) return;
+        // else {
+        //     setAddcart([...Addcart, ...itm]);
+
+        // }
+    }
+    const clicktocarts = (itm) => {
+        const checkcart = state.item_cart.find((itms) => itms.id === itm.id);
+        if (checkcart) {
+            toast.success(`(${itm.no + 1}) ${itm.name} added to the cart`);
+            ++state.totalItem;
+            state.item_cart.map((curElem) => {
+                if (curElem.id === itm.id) {
+                    itm.no = curElem.no + 1;
+                    return { ...curElem, };
+                }
+                return curElem;
+            });
+        }
+        else {
+            state.item_cart.push(itm);
+            toast.success(`${itm.name} added to the cart`);
+            ++state.totalItem;
+            state.item_cart.map((curEleme) => {
+                if (curEleme.id === itm.id) {
+                    itm.no = 1;
+                    return { ...curEleme, };
+                }
+                return curEleme;
+            });
+            //     console.log(Addcart);
+            //     if (Addcart.indexOf(itm) !== -1) return;
+            //     else {
+            //         setAddcart([...Addcart, ...itm]);
+            //     }
+        }
     }
     return (
-        <CartContext.Provider value={{ ...state, IncreMent, DecereSe,AddToCart,showCart, DecrementCart, clicktocart,Addcart ,removefromCart}}>
+        <CartContext.Provider value={{ ...state, IncreMent, DecereSe, clicktocart, clicktocarts, AddToCart, showCart, DecrementCart, removefromCart }}>
             <Navigation />
-            <Toaster/>
-            <ContentItem/>
-            <Content2/>
+            <Toaster />
+            <ContentItem />
+            <Content2 />
             <nav className={cart ? 'ShoppingCartss active' : 'ShoppingCartss'}>
-            <ShoppingCart Addcart={Addcart}/>
+                <ShoppingCart />
             </nav>
         </CartContext.Provider>
     )
